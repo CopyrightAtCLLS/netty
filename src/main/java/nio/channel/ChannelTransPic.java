0a7e9b7c55e8d7cb6f15bfb9306ab0241faffff9
CopyrightAtCLLS <939085525@qq.com>
2021-01-29 22:05:41 +0800

    an awesome image

---
+++ src/main/java/nio/channel/ChannelTransPic.java
@@ -0,0 +1,14 @@
+package nio.channel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 拷贝图片
+ */
+public class ChannelTransPic {
+    public static void main(String[] args) throws IOException {
+        FileInputStream()
+    }
+}
