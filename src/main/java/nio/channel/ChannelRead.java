551ee665286d226adbad9fd55b836611e720fb9c
CopyrightAtCLLS <939085525@qq.com>
2021-01-29 21:10:38 +0800

    FileChannel read

---
+++ src/main/java/nio/channel/ChannelRead.java
@@ -0,0 +1,22 @@
+package nio.channel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelRead {
+    public static void main(String[] args) throws IOException {
+        File file=new File("/tmp/nettyChannel");
+        FileInputStream inputStream = new FileInputStream(file);
+
+        FileChannel channel = inputStream.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate((int) file.length());
+
+        channel.read(buffer);
+        System.out.println(new String(buffer.array()));
+
+        channel.close();
+    }
+}
