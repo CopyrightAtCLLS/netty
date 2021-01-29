1963d03604cebc65133ad1bf6978391b5d687959
CopyrightAtCLLS <939085525@qq.com>
2021-01-29 22:06:13 +0800

    Copy pics

--- src/main/java/nio/channel/ChannelTransPic.java
+++ src/main/java/nio/channel/ChannelTransPic.java
@@ -1,14 +1,25 @@
 package nio.channel;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.FileChannel;
 
 /**
  * 拷贝图片
  */
 public class ChannelTransPic {
     public static void main(String[] args) throws IOException {
-        FileInputStream()
+        FileInputStream inputStream = new FileInputStream("/Users/chenlvlongshen/Desktop/doge.jpg");
+        FileChannel inputStreamChannel = inputStream.getChannel();
+
+        FileOutputStream outputStream = new FileOutputStream("doge.jpg");
+        FileChannel outputStreamChannel = outputStream.getChannel();
+
+        outputStreamChannel.transferFrom(inputStreamChannel,0,inputStreamChannel.size());
+
+        inputStream.close();
+        outputStream.close();
     }
 }
