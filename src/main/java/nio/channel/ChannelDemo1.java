418e5550d2a60730a01bfe164e55dd417cd81e4e
CopyrightAtCLLS <939085525@qq.com>
2021-01-29 20:43:03 +0800

    FileChannel demo

---
+++ src/main/java/nio/channel/ChannelDemo1.java
@@ -0,0 +1,29 @@
+package nio.channel;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 写入数据到本地文件
+ */
+public class ChannelDemo1 {
+    public static void main(String[] args) throws IOException {
+        String str="channel demo";
+        //创建输出流
+        FileOutputStream outputStream = new FileOutputStream("/tmp/nettyChannel");
+        //通过outputStream获取对应的FileChannel
+        FileChannel channel = outputStream.getChannel();
+        //创建ByteBuffer
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        buffer.put(str.getBytes());
+        buffer.flip();
+        channel.write(buffer);
+
+        outputStream.close();
+
+    }
+}
