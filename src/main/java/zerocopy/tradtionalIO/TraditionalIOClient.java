a6de07b8885e7bd44cae46437c61a97339f05e3f
CopyrightAtCLLS <939085525@qq.com>
2021-01-31 16:48:12 +0800

    Traditional IO client

--- src/main/java/zerocopy/tradtionalIO/TraditionalIOClient.java
+++ src/main/java/zerocopy/tradtionalIO/TraditionalIOClient.java
@@ -1,13 +1,34 @@
 package zerocopy.tradtionalIO;
 
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class TraditionalIOClient {
     public static void main(String[] args) throws IOException {
         Socket socket=new Socket("localhost",7001);
         String filename="/Users/chenlvlongshen/Desktop/IMG_6490.jpg";
+        InputStream inputStream=new FileInputStream(filename);
 
+        DataOutputStream dataOutputStream=new DataOutputStream(socket.getOutputStream());
 
+        byte[] bytes=new byte[4096];
+        long length;
+        long total=0;
+
+        long startTime=System.currentTimeMillis();
+
+        while ((length=inputStream.read())>=0){
+            total+=length;
+            dataOutputStream.write(bytes);
+        }
+
+        System.out.println("total size of file : "+total+" bytes\ntime cost : "+(System.currentTimeMillis()-startTime)+"ms");
+
+        dataOutputStream.close();
+        socket.close();
+        inputStream.close();
     }
 }
