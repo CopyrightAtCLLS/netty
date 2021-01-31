ea98f4b19b3532c9a961f09aa7bde4de2725c0bb
CopyrightAtCLLS <939085525@qq.com>
2021-01-31 16:32:41 +0800

    pic transmit

--- src/main/java/zerocopy/tradtionalIO/TraditionalIOClient.java
+++ src/main/java/zerocopy/tradtionalIO/TraditionalIOClient.java
@@ -1,4 +1,13 @@
 package zerocopy.tradtionalIO;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class TraditionalIOClient {
+    public static void main(String[] args) throws IOException {
+        Socket socket=new Socket("localhost",7001);
+        String filename="/Users/chenlvlongshen/Desktop/IMG_6490.jpg";
+
+
+    }
 }
