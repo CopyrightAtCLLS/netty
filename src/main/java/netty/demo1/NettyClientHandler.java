0cb3d74a0ce0bd3a22c4165aa731e54ddcdc5100
CopyrightAtCLLS <939085525@qq.com>
2021-02-01 12:22:28 +0800

    netty client handler

---
+++ src/main/java/netty/demo1/NettyClientHandler.java
@@ -0,0 +1,30 @@
+package netty.demo1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+    //通道就绪时，触发该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Client : "+ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello, 服务器", CharsetUtil.UTF_8));
+    }
+    //当通道有读事件时，触发
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf=(ByteBuf)msg;
+        System.out.println("Data from server : "+buf.toString());
+        System.out.println("Server address : "+ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
