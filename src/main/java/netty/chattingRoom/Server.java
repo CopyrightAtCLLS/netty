1e6739bff7b3bff203eece66ab5a748f8a1b2c31
CopyrightAtCLLS <939085525@qq.com>
2021-02-02 22:42:06 +0800

    server handler

---
+++ src/main/java/netty/chattingRoom/Server.java
@@ -0,0 +1,56 @@
+package netty.chattingRoom;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class Server {
+    private int port;
+    public Server(int port){
+        this.port=port;
+    }
+
+    //run方法处理客户端请求
+    public void run() throws InterruptedException {
+            EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+        try {
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            //获取pipeline
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            //向pipiline加入解码器
+                            pipeline.addLast("decoder", new StringDecoder());
+                            //向pipiline加入编码器
+                            pipeline.addLast("encoder", new StringDecoder());
+                            //加入自定义handler
+                            pipeline.addLast(null);
+                        }
+                    });
+            System.out.println("chantting room Server online...");
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+
+            //监听关闭
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server(7000).run();
+    }
+}
