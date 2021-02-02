8c33679b3234f9c61724da8d6de81c59e4f16b73
CopyrightAtCLLS <939085525@qq.com>
2021-02-02 20:37:22 +0800

    http server

--- src/main/java/netty/http/Server.java
+++ src/main/java/netty/http/Server.java
@@ -2,13 +2,10 @@ package netty.http;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import netty.demo1.NettyServerHandler;
 @SuppressWarnings("all")
 public class Server {
     public static void main(String[] args) throws Exception{
