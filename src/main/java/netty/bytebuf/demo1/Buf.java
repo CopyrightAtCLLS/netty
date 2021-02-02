9029242ac6761f0d376dd3c4a25c546c93937446
CopyrightAtCLLS <939085525@qq.com>
2021-02-02 20:50:43 +0800

    ByteBuf demo1

---
+++ src/main/java/netty/bytebuf/demo1/Buf.java
@@ -0,0 +1,25 @@
+package netty.bytebuf.demo1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+public class Buf {
+    public static void main(String[] args) {
+        //新建ByteBuf,该对象包含一个数组byte[10]
+        ByteBuf buffer = Unpooled.buffer(10);
+
+        for (int i=0;i<10;i++){
+            buffer.writeByte(i);
+        }
+
+        //netty的buffer，不需要使用flip()
+        //底层维护了readerIndex和writerIndex
+        for (int i=0;i<buffer.capacity();i++){
+            System.out.println(buffer.readByte());
+        }
+
+        /*for (int i=0;i<buffer.capacity();i++){
+            System.out.println(buffer.getByte(i));
+        }*/
+    }
+}
