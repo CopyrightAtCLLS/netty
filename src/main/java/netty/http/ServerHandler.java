912663f2c46e91b40c7ab493a3c10487894dd6a9
CopyrightAtCLLS <939085525@qq.com>
2021-02-02 11:55:44 +0800

    http server handler

--- src/main/java/netty/http/ServerHandler.java
+++ src/main/java/netty/http/ServerHandler.java
@@ -7,17 +7,27 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * SimpleChannelInboundHandler是ChannelInboundHandlerAdapter的子类
  * HttpObject表示客户端和服务器通讯的数据被封装为HttpObject
  */
-public class ServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+public class ServerHandler extends SimpleChannelInboundHandler<HttpObject>{
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) {
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception{
         //判断httpObject是不是Httprequest请求
         if (httpObject instanceof HttpRequest) {
             System.out.println("msg类型 = " + httpObject.getClass());
             System.out.println("客户端地址 = " + channelHandlerContext.channel().remoteAddress());
+            //获取请求header
+            HttpRequest httpRequest=(HttpRequest)httpObject;
+            //获取uri
+            URI uri = new URI(((HttpRequest) httpObject).uri());
+            if("/favicon.ico".equals(uri.getPath())){
+                System.out.println("请求了favicon.ico，不做响应");
+                return;
+            }
             //回复信息给浏览器[http]
             ByteBuf content = Unpooled.copiedBuffer("hello，我是服务器", CharsetUtil.UTF_8);
             //构造http响应
