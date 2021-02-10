1db6a8a226fa8fa8c6bade29f83854cc11cb48fe
CopyrightAtCLLS <939085525@qq.com>
2021-02-10 20:45:38 +0800

    WebSocket

--- src/main/java/netty/websocket/Server.java
+++ src/main/java/netty/websocket/Server.java
@@ -59,7 +59,7 @@ public class Server {
             workerGroup.shutdownGracefully();
         }
     }
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        new Server().run();
     }
 }
