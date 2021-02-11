730e265e934180fcb391904465905cf8c44ecb38
CopyrightAtCLLS <939085525@qq.com>
2021-02-12 00:04:17 +0800

    codec

---
+++ src/main/java/codec/protobuf/ServerHandler.java
@@ -0,0 +1,66 @@
+package netty.chattingRoom;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ServerHandler extends SimpleChannelInboundHandler<String> {
+    //定义一个channel组，管理所有的channel
+    //GlobalEventExecutor.INSTANCE是全局的事件执行器，是单例
+    private static ChannelGroup channelGroup=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    //handlerAdded表示连接建立，一旦连接，第一个被执行
+    //将当前channel加入到channelGroup
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel=ctx.channel();
+        //将该客户加入聊天的信息推送给其它客户端
+        //该方法会遍历channelGroup所有channel并发送信息
+        channelGroup.writeAndFlush(simpleDateFormat.format(new Date())+"[客户端] ["+channel.remoteAddress()+"] 加入聊天\n");
+        channelGroup.add(channel);
+    }
+
+    //断开连接时触发
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush(simpleDateFormat.format(new Date())+"[客户端] ["+channel.remoteAddress()+"] 离开房间\n");
+    }
+
+    //表示channel处于活动状态，提示xxx上线
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().remoteAddress()+" online...");
+    }
+
+    //channel处于不活跃状态，提示xxx下线
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().remoteAddress()+" offline...");
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String s) throws Exception {
+        Channel channel = channelHandlerContext.channel();
+        //排除自己，进行广播
+        channelGroup.forEach(ch -> {
+            if(channel!=ch){
+                ch.writeAndFlush("[客户端] ["+channel.remoteAddress()+"] : "+s+"\n");
+            }else {
+                ch.writeAndFlush("self : "+s+"\n");
+            }
+        });
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
